3d79bcbe87479444581bdbb891eddb038b23b026
Visual studio by Akulov <vspc_vs@VSPC34.RU>
2021-04-05 12:43:36 +0400

    Добавьте файлы проекта.

---
+++ Akulov_Lr4/Akulov_Lr4.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <list>
+#include <locale.h>
+#include <iterator>
+
+using namespace std;
+
+int main()
+{	
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	list<int> List;
+	list<int> minus;
+	list<int> plus;
+	list<int>::iterator Iter;
+	for (int i = 0; i < 10; i++)
+	{
+		List.push_back(rand() % 21 - 10);
+	}
+	for (Iter = List.begin(); Iter != List.end(); Iter++)
+	{
+		if (*Iter < 0)
+		{
+			minus.push_back(*Iter);
+		}
+		else
+			plus.push_back(*Iter);
+	}
+	cout << "Отрицательные:\n";
+	for (Iter = minus.begin(); Iter != minus.end(); Iter++)
+	{
+		cout << *Iter << endl;
+	}
+	cout << "\nПоложительные:\n";
+	for (Iter = plus.begin(); Iter != plus.end(); Iter++)
+	{
+		cout << *Iter << endl
+	}
+}
+
